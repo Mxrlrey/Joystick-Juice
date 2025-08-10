62a343ab9e664e4dc031ba0f4a59213b216af120
Djavantl <djavanlopesteixeira@gmail.com>
2025-08-10 10:48:16 -0300

    Criação do template base

--- app/joystickjuice/settings.py
+++ app/joystickjuice/settings.py
@@ -58,10 +58,13 @@ ROOT_URLCONF = 'joystickjuice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # pasta templates na raiz
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
