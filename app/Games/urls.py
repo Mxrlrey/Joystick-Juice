581e3464fd31460941be64a35c7782e9911bac38
Marley <marleyextreme02@gmail.com>
2025-08-11 20:11:26 -0300

    Adicao da API, dados de games inseridos. IGBD

--- app/Games/urls.py
+++ app/Games/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # /url no browser; a função resposável; nome da url
     path('home/', views.home, name='home'),
+    path('listar/', views.listar_jogos, name='listar_jogos'),
+    path('preencher/', views.preencher_e_salvar, name='preencher_e_salvar'),
 ]
