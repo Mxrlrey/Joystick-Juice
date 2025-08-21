119f2b16cba5657fc04df0132be704a81b9c0f9c
Djavantl <djavanlopesteixeira@gmail.com>
2025-08-20 23:26:37 -0300

    ajustando html e css da navbar

--- app/Accounts/forms.py
+++ app/Accounts/forms.py
@@ -11,9 +11,22 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['name','username', 'email', "password1", "password2", 'birthdate', 'gender', 'avatar']
         widgets = {
-            'birthdate': forms.DateInput(attrs={'type': 'date'}),
-            'gender': forms.Select(),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome completo'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome de usuário'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Digite sua Senha'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Digite sua Senha novamente'}),
+            'birthdate': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'},format='%Y-%m-%d' ),
+            'gender': forms.Select(attrs={'class': 'form-control'}),
+            'avatar': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = ''
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Digite sua senha'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Digite sua senha novamente'})
 
 class UserFormUpdate(forms.ModelForm):
     avatar = forms.ImageField(widget=CustomClearableFileInput, required=False)
@@ -21,9 +34,14 @@ class UserFormUpdate(forms.ModelForm):
         model = User
         fields = ['name','username', 'email', 'birthdate', 'gender', 'avatar']
         widgets = {
-            'birthdate': forms.DateInput(
-                attrs={'type': 'date', 'class': 'form-control'},  # type date + styling
-                format='%Y-%m-%d' 
-            ),
-            'gender': forms.Select(),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome completo'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome de usuário'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'}),
+            'birthdate': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'},format='%Y-%m-%d' ),
+            'gender': forms.Select(attrs={'class': 'form-control'}),
+            'avatar': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
+        help_texts = {
+            'username': '', 
+            'email': '',
         }
