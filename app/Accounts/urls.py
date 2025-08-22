4e6c2e5a21ea1469c1f4526b7028ef92de9fbdcf
Marley <marleyextreme02@gmail.com>
2025-08-22 13:34:20 -0300

    Adicao de stats list e reviews/comentarios no perfil do usuario

--- app/Accounts/urls.py
+++ app/Accounts/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
-    # /url no browser; a função resposável; nome da url
     path('register/', views.register_view, name='user_register'),
     path('login/', views.login_view, name='user_login'),
     path('logout/', views.LogoutUserView.as_view(), name='user_logout'),
@@ -12,6 +9,8 @@ urlpatterns = [
     path('delete/<int:pk>/', views.UserDeleteView.as_view(), name='user_delete'),
     path('update/<int:pk>/', views.UserUpdateView.as_view(), name='user_update'),
     path('bio/<int:pk>/', views.UpdateBioView.as_view(), name='user_bio'),
-    path('profile/<int:pk>/', views.PerfilUserView.as_view(), name="user_profile")
-
+    path('profile/<int:pk>/', views.PerfilUserView.as_view(), name="user_profile"),
+    path('profile/<int:pk>/reviews/', views.user_reviews, name="user_reviews"),
+    path('profile/<int:pk>/comments/', views.user_comments, name="user_comments"),
+    path('profile/<int:pk>/games/<str:status>/', views.user_game_list, name="user_game_list"),
 ]
--- app/Accounts/views.py
+++ app/Accounts/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView, ListView, DeleteView, UpdateView, DetailView
 from .models import User
+from Games.models import UserGameList
+from Reviews.models import Review, Comment
 from . import forms
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
@@ -10,6 +12,7 @@ from django.contrib.auth.views import LogoutView
 from django.contrib import messages
 from django.views.decorators.cache import never_cache
 from django.utils.decorators import method_decorator
+
 
 
 # Create your views here.
@@ -75,7 +78,7 @@ class UpdateBioView(LoginRequiredMixin, UpdateView):
 
 @method_decorator(never_cache, name='dispatch')
 class PerfilUserView(LoginRequiredMixin, DetailView):
-    model= User
+    model = User
     template_name = 'user_profile.html'
     login_url = reverse_lazy('user_login')
 
@@ -83,6 +86,67 @@ class PerfilUserView(LoginRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         context['update_form'] = forms.UserFormUpdate(instance=self.object)
         context['bio_form'] = forms.UserFormBio(instance=self.object)
+        context['reviews_count'] = Review.objects.filter(user=self.object).count()
+        context['comments_count'] = Comment.objects.filter(user=self.object).count()
+
+        game_list = UserGameList.objects.filter(user=self.object)
+        context['stats'] = {
+            "para_jogar": game_list.filter(status="P").count(),
+            "jogando": game_list.filter(status="J").count(),
+            "concluido": game_list.filter(status="C").count(),
+            "abandonado": game_list.filter(status="A").count(),
+            "total": game_list.count(),
+        }
+
         return context
+    
+@login_required
+def user_game_list(request, pk, status):
+    user = User.objects.get(pk=pk)
+
+    if status == "J":
+        jogos = UserGameList.objects.filter(user=user, status="J")
+        titulo = "Jogando"
+    elif status == "C":
+        jogos = UserGameList.objects.filter(user=user, status="C")
+        titulo = "Concluídos"
+    elif status == "A":
+        jogos = UserGameList.objects.filter(user=user, status="A")
+        titulo = "Abandonados"
+    elif status == "P":
+        jogos = UserGameList.objects.filter(user=user, status="P")
+        titulo = "Para Jogar"
+    elif status == "T":
+        jogos = UserGameList.objects.filter(user=user)
+        titulo = "Todos os Jogos"
+    else:
+        jogos = UserGameList.objects.none()
+        titulo = "Desconhecido"
+
+    return render(request, "user_game_list.html", {
+        "user_profile": user,
+        "jogos": jogos,
+        "titulo": titulo
+    })
+
+
+@login_required
+def user_reviews(request, pk):
+    user = User.objects.get(pk=pk)
+    reviews = Review.objects.filter(user=user)
+    return render(request, "user_reviews.html", {
+        "user_profile": user,
+        "reviews": reviews
+    })
+
+
+@login_required
+def user_comments(request, pk):
+    user = User.objects.get(pk=pk)
+    comments = Comment.objects.filter(user=user)
+    return render(request, "user_comments.html", {
+        "user_profile": user,
+        "comments": comments
+    })
 
 
